58df895a3300353d7e28ca9c4b10629be03a3dc5
Asia K <asialakaygrady@gmail.com>
2022-03-02 04:39:41 -0500

    updated bio photo link

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.jpg"
+        src="https://raw.githubusercontent.com/asiakay/til/master/src/images/profile-pic.JPG"
         width={50}
         height={50}
         quality={95}
